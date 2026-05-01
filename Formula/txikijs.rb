84beac765ef8e33a42084edfb3a1637a97fa4290
방성범 (Bang Seongbeom) <bangseongbeom@gmail.com>
2026-05-02 03:26:24 +0900

    Fix root_url in txikijs formula to point to the new repository

--- Formula/txikijs.rb
+++ Formula/txikijs.rb
@@ -9,7 +9,7 @@ class Txikijs < Formula
   head "https://github.com/saghul/txiki.js.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/bangseongbeom/homebrew-txikijs/releases/download/txikijs-26.4.0"
+    root_url "https://github.com/bangseongbeom/homebrew-tap/releases/download/txikijs-26.4.0"
     sha256 cellar: :any_skip_relocation, arm64_tahoe:  "b7b9586ac809245fc3b41f3221b04675bc8af9f26d869991a63c1ad38caacc69"
     sha256 cellar: :any_skip_relocation, sequoia:      "5a7af7e6d0020288a6f1ab8ea0e52478cc924e84067cb9af47b18d491922c69d"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "2b91587621ee43d6baf979c884eff78460b4895735b3d954dfe717429abeade2"
